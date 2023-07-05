2228a987435abb4a8fe1118f41c86efe068d49e2
Boban <93476360+Taia25S@users.noreply.github.com>
2023-07-05 17:36:21 +0200

    Add admin project

---
+++ Cinema-Ticketing-System-Admin-181550/CinemaTicketingSystemAdmin_181550/Models/CinemaApplicationUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CinemaTisketingSystemAdmin_191010.Models
+{
+    public class CinemaApplicationUser
+    {
+        public string Email { get; set; }
+        public string NormalizedEmail { get; set; }
+        public string Username { get; set; }
+        public string NormalizedUsername { get; set; }
+
+    }
+}
---
+++ Cinema-Ticketing-System-Admin-181550/CinemaTicketingSystemAdmin_181550/Models/TicketsInOrder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CinemaTisketingSystemAdmin_191010.Models
+{
+    public class TicketsInOrder
+    {
+        public int TicketId { get; set; }
+        public Ticket Ticket { get; set; }
+        public int OrderId { get; set; }
+        public Order Order { get; set; }
+        public int Quantity { get; set; }
+
+    }
+}
